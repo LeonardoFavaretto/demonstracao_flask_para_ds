fb21425dee38cd314a0f2e5974e497b7cfdde5d1
Leonardo <leonardo_fco@hotmail.com>
2020-08-31 21:57:50 -0300

    remove unused files

--- ds_api_on_docker/config.py
+++
@@ -1,8 +0,0 @@
-from os import path
-
-import ds_api_on_docker
-
-base_path = path.dirname(path.dirname(ds_api_on_docker.__file__))
-workspace_path = path.join(base_path, 'workspace')
-data_path = path.join(workspace_path, 'data')
-models_path = path.join(workspace_path, 'models')
